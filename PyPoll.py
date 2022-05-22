6f5f358e3784ca272e16d9797352ab682823b95b
msshahid21 <muhammad.s.shahid@outlook.com>
2022-05-21 20:51:22 -0400

    Add resources folder and move Raw Data to resources, Add Pseudocode to Python Script

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+# The data that needs to be retrieved:
+# 1. The total number of votes cast
+# 2. A complete list of candidates who recieved votes
+# 3. The percentage votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
